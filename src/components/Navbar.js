eea6041839492f2c8ec5019009f3dd86c270c952
kbaskerville <klbaskerille0520@gmail.com>
2021-07-26 13:17:59 -0700

    added logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,20 @@
-import React from 'react'
+import React from "react";
+import logo from "../Logo.png";
+
+
 
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="/">Web Dev Portfolio</a>
+            <a className="navbar-brand" href="/"><img className="logo" src={logo} alt="logo..." /> Ohz's Portfolio </a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
+                <ul className="navbar-nav ml-auto">
                     <li className="nav-item active">
-                        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                        <a className="nav-link" href="/">Home</a>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href="/">Link</a>
