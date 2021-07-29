0552b29d8a456de94a6488b04f3165a2777d0a06
kbaskerville <klbaskerille0520@gmail.com>
2021-07-28 22:38:32 -0700

    about page added with picture

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import About from "./components/About";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <>
     <Navbar />
     <Header />
+    <About />
     </>
   ); 
 }
---
+++ src/components/About.js
@@ -0,0 +1,23 @@
+import React from "react"
+import aboutImage from "../IMG_0966_3.jpg";
+
+const About = () => {
+    return (
+        <div className="container py-5" >
+            <div className="row">
+            <div className="col-lg-6 col-xm-12">
+                <div className="photo-wrap mb-5">
+                    <img className="aboutimage" src={aboutImage} alt="aboutimage" />
+
+                </div>
+            </div>
+            <div className="col-lg-6 col-xm-12">
+                <h1 className="about-heading"> About Me</h1>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis numquam soluta labore ea minus doloribus magni deleniti consequatur molestiae at? Magni minus, est culpa quibusdam ipsa quis! Quo, doloremque sed?</p>
+            </div>
+            </div> 
+        </div>
+    )
+}
+
+export default About
