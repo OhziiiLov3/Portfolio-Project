c2cd1c04441c7d9ee5b11653805141013134f429
kbaskerville <klbaskerille0520@gmail.com>
2021-07-29 00:22:01 -0700

    added services section with icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,10 +23,10 @@ const Navbar = () => {
                         <a className="nav-link" href="/">About</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/">Projects</a>
+                        <a className="nav-link" href="/">Services</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/">Services</a>
+                        <a className="nav-link" href="/">Projects</a>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href="/">Contact</a>
---
+++ src/components/Services.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faDesktop, faHeadphones, faTint} from '@fortawesome/free-solid-svg-icons'
+
+
+const Services = () => {
+    return (
+        <div className="services">
+            <h1 className="py-4"><h1>My Services</h1>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-4 col-sm-7">
+                            <div className="box">
+                                <div className="circle"> <FontAwesomeIcon icon ={faDesktop} size="2x"/> </div>
+                                <h3> Web Development</h3>
+                                <p> Lorem Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                        </div>
+                        {/*  */}
+                        <div className="col-md-4 col-sm-7">
+                            <div className="box">
+                                <div className="circle"> <FontAwesomeIcon icon={faHeadphones} size="2x" /> </div>
+                                <h3> Audio Engineering</h3>
+                                <p> Lorem Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
+                            </div>
+                        </div>
+                        {/*  */}
+                        <div className="col-md-4 col-sm-7">
+                            <div className="box">
+                                <div className="circle"> <FontAwesomeIcon icon={faTint} size="2x" /> </div>
+                                <h3>Zooty Water</h3>
+                                <p> Lorem Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
+                            </div>
+                        </div>
+                        {/*  */}
+                    </div>
+                </div>
+            </h1>
+        </div>
+    )
+}
+
+export default Services;
