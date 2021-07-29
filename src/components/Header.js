ce901a3f2fdbc7718f5230806018b493fc145c02
kbaskerville <klbaskerille0520@gmail.com>
2021-07-28 21:47:41 -0700

    added header and styled

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react"
+import Typed from "react-typed"
+
+const Header = () => {
+    return (
+        <div className="header-wrapper">
+            <div className="main-info">
+                <h1> Web Development Portfolio and Services</h1>
+                <Typed
+                    className="typed-text"
+                    strings={[" Entreprneur ", "Full Stack Software Engineer ", " Audio Engineer " , " and Alkaline Water Enthusiast " ]}
+                    typeSpeed={40}
+                    backSpeed={60}
+                    loop
+                />
+                <a href="#" className="btn-main"> Contact me</a>
+            </div>
+        </div>
+    )
+}
+
+export default Header
