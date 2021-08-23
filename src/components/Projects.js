2437d1b9e6e7c6ab030bdc083b8438a5e0fdc7b1
ohziiilov3 <klbaskerville0520@gmail.com>
2021-08-23 15:18:05 -0700

    popboxes added to each project with working links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -38,13 +38,13 @@ const Projects = () => {
 
 //     }
 
-    // Netflix
+    // Zooty Water APP
     const openPopupboxZootywater = () => {
         const content = (
             <>
                 <img className="project-image-popupbox" src={zootyWater} alt="ZootyWater Project..." />
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex cumque illo est expedita quos adipisci suscipit unde itaque qui perferendis.</p>
-                <b>Zooty Water:</b> <a className="hyper-link" onClick={() => window.open("https://zootywaterapp.herokuapp.com")}>https://zootywaterapp.herokuapp.com</a>
+                <b>DEMO:</b> <a className="hyper-link" onClick={() => window.open("https://zootywaterapp.herokuapp.com")}>https://zootywaterapp.herokuapp.com</a>
             </>
         )
         PopupboxManager.open({ content })
@@ -52,7 +52,7 @@ const Projects = () => {
         content,
         config: {
             titleBar: {
-                text: "Your Edited Text Here",
+                text: "Zooty Water App",
             },
         },
     });
@@ -62,6 +62,93 @@ const Projects = () => {
         titleBar: {
             enable: true,
             text: "Zooty Water App project."
+        },
+        fadeIn: true,
+        fadeInSpeed: 500
+    }
+
+    // Housing 4 U
+    const openPopupboxHousing4u = () => {
+        const content = (
+            <>
+                <img className="project-image-popupbox" src={housing4u} alt="Housing4U Project..." />
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex cumque illo est expedita quos adipisci suscipit unde itaque qui perferendis.</p>
+                <b>DEMO:</b> <a className="hyper-link" onClick={() => window.open("https://housing4you.herokuapp.com" , "_blank")}> https://housing4you.herokuapp.com</a>
+            </>
+        )
+        PopupboxManager.open({ content })
+        PopupboxManager.update({
+        content,
+        config: {
+            titleBar: {
+                text: "Housing4U",
+            },
+        },
+    });
+    }
+
+    const popupboxConfigHousing4u = {
+        titleBar: {
+            enable: true,
+            text: "Housing4U project."
+        },
+        fadeIn: true,
+        fadeInSpeed: 500
+    }
+
+    // Wayfarer
+    const openPopupboxWayfarer = () => {
+        const content = (
+            <>
+                <img className="project-image-popupbox" src={wayfarer} alt="Wayfarer Project..." />
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex cumque illo est expedita quos adipisci suscipit unde itaque qui perferendis.</p>
+                <b>DEMO:</b> <a className="hyper-link" onClick={() => window.open("https://project2-wayfarer.herokuapp.com/", "_blank")}> https://project2-wayfarer.herokuapp.com/</a>
+            </>
+        )
+        PopupboxManager.open({ content })
+        PopupboxManager.update({
+            content,
+            config: {
+                titleBar: {
+                    text: "Wayfarer",
+                },
+            },
+        });
+    }
+
+    const popupboxConfigWayfarer = {
+        titleBar: {
+            enable: true,
+            text: "Wayfarer Project."
+        },
+        fadeIn: true,
+        fadeInSpeed: 500
+    }
+
+    // DigiPet
+    const openPopupboxDigipet = () => {
+        const content = (
+            <>
+                <img className="project-image-popupbox" src={digipet} alt="Wayfarer Project..." />
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex cumque illo est expedita quos adipisci suscipit unde itaque qui perferendis.</p>
+                <b>DEMO:</b> <a className="hyper-link" onClick={() => window.open("https://pages.git.generalassemb.ly/kbaskerville/ZootyMon/", "_blank")}> https://pages.git.generalassemb.ly/kbaskerville/ZootyMon//</a>
+            </>
+        )
+        PopupboxManager.open({ content })
+        PopupboxManager.update({
+            content,
+            config: {
+                titleBar: {
+                    text: "Digipet",
+                },
+            },
+        });
+    }
+
+    const popupboxConfigDigipet = {
+        titleBar: {
+            enable: true,
+            text: "DigiPet."
         },
         fadeIn: true,
         fadeInSpeed: 500
@@ -78,19 +165,19 @@ const Projects = () => {
                             <FontAwesomeIcon className="project-icon" icon={faSearchPlus} />
                     </div>
                     {/*  */}
-                    <div className="project-image-box">
+                    <div className="project-image-box" onClick={openPopupboxHousing4u}>
                         <img className="project-image" src={housing4u} alt="Housing4u Project" />
                         <div className="overflow"></div>
                             <FontAwesomeIcon className="project-icon" icon={faSearchPlus} />
                     </div>
                     {/*  */}
-                    <div className="project-image-box">
+                    <div className="project-image-box" onClick={openPopupboxWayfarer}>
                         <img className="project-image" src={wayfarer} alt="Wayfarer Project" />
                         <div className="overflow"></div>
                             <FontAwesomeIcon className="project-icon" icon={faSearchPlus} /> 
                     </div>
                     {/*  */}
-                    <div className="project-image-box">
+                    <div className="project-image-box" onClick={openPopupboxDigipet}>
                         <img className="project-image" src={digipet} alt="Digipet Project"/> 
                         <div className="overflow"></div>
                             <FontAwesomeIcon className="project-icon" icon={faSearchPlus} />
@@ -98,6 +185,9 @@ const Projects = () => {
                 </div>
             </div>
             <PopupboxContainer {...popupboxConfigZootyWater} />
+            <PopupboxContainer {...popupboxConfigHousing4u} />
+            <PopupboxContainer {...popupboxConfigWayfarer} />
+            <PopupboxContainer {...popupboxConfigDigipet} />
         </div>
     
     )
