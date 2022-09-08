6fd2755a76be24974e82cced5e22cdf3e40c0c4a
ohziiilov3 <klbaskerville0520@gmail.com>
2022-09-07 17:27:54 -0700

    push

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-
-import './App.css';
+import React from "react";
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import About from "./components/About";
-import Services from "./components/Services"
+import Services from "./components/Services";
 import Experience from "./components/Experience";
-import Projects from "./components/Projects"
-import Contacts from "./components/Contacts"
-import Footer from "./components/Footer"
-
+import Projects from "./components/Projects";
+import Contacts from "./components/Contacts";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-    <Navbar />
-    <Header />
-    <About />
-    <Services />
-    <Experience />
-    <Projects />
-    <Contacts />
-    <Footer />
+      <Navbar />
+      <Header />
+      <About />
+      <Services />
+      <Experience />
+      <Projects />
+      <Contacts />
+      <Footer />
     </>
-  ); 
+  );
 }
 
 export default App;
